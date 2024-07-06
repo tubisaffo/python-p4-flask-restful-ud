28363e40f3188c643247244633e21926f2d70091
tubisaffo <tubisaffo@gmail.com>
2024-07-06 08:55:50 +0300

    update/delete restful api

--- server/app.py
+++ server/app.py
@@ -64,6 +64,7 @@ class Newsletters(Resource):
         )
 
         return response
+    
 
 api.add_resource(Newsletters, '/newsletters')
 
@@ -76,6 +77,39 @@ class NewsletterByID(Resource):
         response = make_response(
             response_dict,
             200,
+        )
+
+        return response
+    
+    def patch(self, id):
+
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+    def delete(self, id):
+
+        record = Newsletter.query.filter(Newsletter.id == id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
         )
 
         return response
